6933137e243395f2511fd96c9b8a05400653526a
Mafalda <mafalda.villazan@gmail.com>
2019-08-18 18:13:48 -0700

    Adding controllers models and routes to connect with the DB

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,16 @@
 import axios from "axios";
 
 export default {
-  getBooks: function(query) {
-    return axios.get("/api/books", { params: { q: query } });
+  // Gets all books
+  getBooks: function() {
+    return axios.get("/api/books").then(results => results.data);
+  },
+  // Deletes the book with the given id
+  deleteBook: function(id) {
+    return axios.delete("/api/books/" + id).then(results => results.data);
+  },
+  // Saves a book to the database
+  saveBook: function(bookData) {
+    return axios.post("/api/books", bookData).then(results => results.data);
   }
 };
--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 const express = require("express");
+const app = express();
+const routes = require("./routes/api");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
-const app = express();
+const mongoose = require("mongoose");
 
-// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
+app.use(routes);
 
-// Send every other request to the React app
-// Define any API routes before this runs
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googleBooks");
+
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
